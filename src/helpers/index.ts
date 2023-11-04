b38752a82eb249c4793b13a7b6df9d1184d8fa18
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2023-11-05 00:43:22 +0530

    Sixth commit

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -8,6 +8,6 @@ export const random = () => crypto.randomBytes(128).toString('base64');
 
 //create authentication 
 export const authentication = (salt:string, password:string) =>{
-    return crypto.createHmac('sha256',[salt,password].join('/')).update(SECRET).digest
+    return crypto.createHmac('sha256',[salt,password].join('/')).update(SECRET).digest('hex')
 }
 
